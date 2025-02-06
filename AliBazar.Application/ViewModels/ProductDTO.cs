cd200c5094cfe2ab07582b99a602853ebd0910b8
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-02-06 23:36:39 +0500

    aa

--- AliBazar.Application/ViewModels/ProductDTO.cs
+++ AliBazar.Application/ViewModels/ProductDTO.cs
@@ -9,7 +9,7 @@ namespace AliBazar.Application.ViewModels
         public string? DescriptionUz { get; set; }
         public string? DescriptionRuss { get; set; }
         public required decimal Price { get; set; }
-        public required decimal PreviousPrice { get; set; }
+        public decimal? PreviousPrice { get; set; }
         public required long CategoryId { get; set; }
         public List<IFormFile?> ImageUrl { get; set; }
     }
--- AliBazar.Application/ViewModels/ProductViewModel.cs
+++ AliBazar.Application/ViewModels/ProductViewModel.cs
@@ -8,7 +8,7 @@ namespace AliBazar.Application.ViewModels
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required decimal Price { get; set; }
-        public required decimal PreviousPrice { get; set; }
+        public decimal? PreviousPrice { get; set; }
         public List<string>? ImageUrl { get; set; }
         public ProductDetail? ProductDetails { get; set; }
 
